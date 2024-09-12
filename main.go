f612ecaa4f5f28a8263356837115816b98a2eeda
Roy Golan <rgolan@redhat.com>
2024-09-12 14:35:24 +0300

    add checks
    Signed-off-by: Roy Golan <rgolan@redhat.com>

--- main.go
+++ main.go
@@ -6,10 +6,7 @@ import (
 )
 
 func main() {
-    fmt.Println("casbin test")
-
     e := casbin.NewEnforcer("model.conf", "policy.csv")
     fmt.Println(e.Enforce("user:default/guest", "orchestrator.workflows.foo", "read"))
-    
-
+    fmt.Println(e.Enforce("user:default/guest", "orchestrator.foo", "read"))
 }
--- main.js
+++ main.js
@@ -1,12 +1,10 @@
 async function main() {
     const { newEnforcer } = require('casbin');
-    // For browser:
-    //import { newEnforcer } from 'casbin';
 
     const enforcer = await newEnforcer('model.conf', 'policy.csv');
 
     console.log(await enforcer.enforce("user:default/guest", "orchestrator.workflows.foo", "read"));
+    console.log(await enforcer.enforce("user:default/guest", "orchestrator.foo", "read"));
 }
-
 
 main();
